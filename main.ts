5c11f3776b48de89acabe4a7b87efa47ae1549a0
juhyoungRyu <thinkoutside2004@gmail.com>
2023-12-19 22:03:47 +0900

    최초 실행 및 유저 인증 등 실제 메인 프로세스 진행을 할 수 있게 변형

--- main.ts
+++ main.ts
@@ -1,6 +1,35 @@
-import { UserService } from "./service/UserService";
-import { userRepo } from "./db/demoDB";
+import Game from "inquirer";
+import LoginService from "./service/LoginService";
+import { PromptParam } from "./interface/prompt";
+import { userRepo } from "./db/userDB";
 
-const userService = new UserService(userRepo);
+async function GamePrompt({ name, message, type, choices }: PromptParam) {
+  const result = await Game.prompt({
+    name,
+    message,
+    type,
+    choices,
+  });
 
-userService.addUser([{}, {}, {}]);
+  return result;
+}
+
+async function main(): Promise<void> {
+  console.log("⚔️  Fantasy Story 🛡️");
+
+  const { isUser } = await Game.prompt({
+    name: "isUser",
+    message: "Did you play it before?",
+    type: "list",
+    choices: [
+      { name: "Yes", value: true },
+      { name: "No", value: false },
+    ],
+  });
+
+  const isLogin = await LoginService(isUser, GamePrompt, userRepo);
+
+  console.log(isLogin.data);
+}
+
+main();
